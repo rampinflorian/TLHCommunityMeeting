0c740b361dc7ad4930b518f68bf782a7919ad685
Florian Rampin <contact@florianrampin.fr>
2022-04-18 12:59:34 +0200

    Add history meeting

---
+++ TLHCommunityMeeting/Controllers/HistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TLHCommunityMeeting.Data;
+
+namespace TLHCommunityMeeting.Controllers;
+
+[Route("history")]
+public class HistoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HistoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    [Route("")]
+    public IActionResult Index()
+    {
+        return View(_context.Questions.Where(m => m.MeetingAt != null).OrderBy(m => m.CreatedAt).ToList());
+    }
+}
